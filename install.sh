780a08105af43f44fb788b28900fb2fcb8a45d83
Mike Wyatt <wyatt.mike@gmail.com>
2013-11-28 17:02:11 -0400

    working install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
 DOTFILES_ROOT=$(pwd)
-ALL_DOTFILES=$DOTFILES_ROOT/*
+#ALL_DOTFILES=$DOTFILES_ROOT/*.*
+ALL_DOTFILES=$(find . -maxdepth 1   \
+  -not -name '.'                    \
+  -not -name 'examples'             \
+  -not -name README.md              \
+  -not -name bootstrap.sh           \
+  -not -name resync.sh              \
+  -not -name bootstrap.sh~          \
+  -not -name install.sh             \
+  -not -name recover-submodules.sh)
 
 for DOT_FILE in $ALL_DOTFILES; do
   case $DOT_FILE in
@@ -10,8 +19,8 @@ for DOT_FILE in $ALL_DOTFILES; do
     *)
       MOVE_OLD=0
       PERFORM=1
-      FILE_NAME=$(basename $DOT_FILE)
-      TARGET_FILE=$HOME/.$FILE_NAME
+      FILE_NAME=$(basename "$DOT_FILE")
+      TARGET_FILE="$HOME/$FILE_NAME"
 
       if [ -e $TARGET_FILE ]; then
         if [ $DOT_FILE -ef $TARGET_FILE ]; then
@@ -23,9 +32,9 @@ for DOT_FILE in $ALL_DOTFILES; do
 
       if [ 0 -lt $MOVE_OLD ]; then
         echo Moving $TARGET_FILE to $TARGET_FILE.old
-        mv $TARGET_FILE $TARGET_FILE.old
+        mv "$TARGET_FILE" "$TARGET_FILE.old"
       fi
 
-      [ 0 -lt $PERFORM ] && ln -s $DOT_FILE $TARGET_FILE
+      [ 0 -lt $PERFORM ] && ln -s "$DOTFILES_ROOT/$FILE_NAME" "$TARGET_FILE"
   esac
 done
